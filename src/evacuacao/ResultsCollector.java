20bebf7df83c6bda4585d344f031146678046c9f
Rui Costa <ei12150@fe.up.pt>
2017-11-07 12:45:04 +0000

    Results Collector

--- src/evacuacao/ResultsCollector.java
+++ src/evacuacao/ResultsCollector.java
@@ -10,19 +10,19 @@ import sajas.core.Agent;
 import sajas.core.behaviours.CyclicBehaviour;
 
 public class ResultsCollector extends Agent {
-	
+
 	private int livesSaved = 0;
 
 	private Codec codec;
 	private Ontology serviceOntology;
-	
+
 	public ResultsCollector() {
-	
+
 	}
-	
+
 	@Override
 	public void setup() {
-		
+
 		// register language and ontology
 		codec = new SLCodec();
 		serviceOntology = ServiceOntology.getInstance();
@@ -32,21 +32,19 @@ public class ResultsCollector extends Agent {
 		// results listener
 		addBehaviour(new ResultsListener());
 	}
-	
+
 	private class ResultsListener extends CyclicBehaviour {
 
 		private static final long serialVersionUID = 1L;
-		
-		private MessageTemplate template = 
-				MessageTemplate.and(
-						MessageTemplate.MatchPerformative(ACLMessage.INFORM),
-						MessageTemplate.MatchOntology(ServiceOntology.ONTOLOGY_NAME));
+
+		private MessageTemplate template = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+				MessageTemplate.MatchOntology(ServiceOntology.ONTOLOGY_NAME));
 
 		@Override
 		public void action() {
-			
+
 			// when evacuation is complete
 		}
-			
+
 	}
 }
